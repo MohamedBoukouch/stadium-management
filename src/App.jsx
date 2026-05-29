15340da42fdb1109e23cbcfb978d570e2bfe052c
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2026-05-29 12:09:19 +0100

    first commit by bkch

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+<p class="text-red-500">Hello</p>
+    </>
+  )
+}
+
+export default App
