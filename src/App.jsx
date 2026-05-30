955055f7a16171a4d978d1a62a59e85c6e9f2b10
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2026-05-30 17:35:31 +0100

    solve orb of paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Client Pages
 import ClientLayout from "./layouts/ClientLayout";
-// import Home from "./pages/client/Home";
+import Home from "./pages/client/Home";
 import Reservation from "./pages/client/Reservation";
 
 // Admin Pages
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClientLayout />}>
-          {/* <Route index element={<Home />} /> */}
+          <Route index element={<Home />} />
           <Route path="reservation" element={<Reservation />} />
         </Route>
 
