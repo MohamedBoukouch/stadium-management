b777c497b89f5ccbbbf1639fd3973c4de356ab90
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2026-05-30 17:23:02 +0100

    fix prb of paths

--- src/layouts/ClientLayout.jsx
+++ src/layouts/ClientLayout.jsx
@@ -1,11 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 function ClientLayout() {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 }
 
 export default ClientLayout;
